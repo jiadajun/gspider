cac81aaf2af9b57694bcab0329eb320856ddae07
jiadajun <1228019939@qq.com>
2022-11-28 10:13:43 +0800

    配置信息

--- common/data_struct.go
+++ common/data_struct.go
@@ -1,1 +1,11 @@
 package common
+
+type DbStruct struct {
+	User           string `json:"user"`
+	Password       string `json:"password"`
+	Host           string `json:"host"`
+	Port           int64  `json:"port"`
+	Database       string `json:"database"`
+	ConnectTimeout string `json:"connect_timeout"`
+	Charset        string `json:"charset"`
+}
--- common/settings.go
+++ common/settings.go
@@ -1,1 +1,28 @@
 package common
+
+import (
+	"github.com/go-redis/redis"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var LocalSpider = &DbStruct{
+	User:           "root",
+	Password:       "123",
+	Host:           "127.0.0.1",
+	Port:           3306,
+	Database:       "spider",
+	ConnectTimeout: "10s",
+	Charset:        "utf8",
+}
+
+var AtSpider = &DbStruct{
+	User:           "at",
+	Password:       "at111",
+	Host:           "192.168.2.114",
+	Port:           3306,
+	Database:       "1233",
+	ConnectTimeout: "10s",
+	Charset:        "utf8",
+}
+
+var RedisClient1 = redis.NewClient(&redis.Options{Addr: "111111111118:6379", Password: "111111111111"})
